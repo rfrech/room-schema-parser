96d0040217fca9e64c5ea7461fe5249f877f455e
rfrech <rfrechette@cnm.edu>
2020-11-04 11:54:24 -0700

    Added support for View DDL

--- src/edu/cnm/deepdive/model/Database.java
+++ src/edu/cnm/deepdive/model/Database.java
@@ -8,11 +8,22 @@ public class Database {
   @Expose
   private List<Entity> entities;
 
+  @Expose
+  private List<View> views;
+
   public List<Entity> getEntities() {
     return entities;
   }
 
   public void setEntities(List<Entity> entities) {
     this.entities = entities;
+  }
+
+  public List<View> getViews() {
+    return views;
+  }
+
+  public void setViews(List<View> views) {
+    this.views = views;
   }
 }
--- src/edu/cnm/deepdive/model/Schema.java
+++ src/edu/cnm/deepdive/model/Schema.java
@@ -5,7 +5,9 @@ import java.util.stream.Stream;
 
 public class Schema {
 
-  private static final String PLACEHOLDER = "${TABLE_NAME}";
+  private static final String ENTITY_PLACEHOLDER = "${TABLE_NAME}";
+  private static final String VIEW_PLACEHOLDER = "${VIEW_NAME}";
+
   @Expose
   private Database database;
 
@@ -18,14 +20,19 @@ public class Schema {
   }
 
   public Stream<String> getDdl() {
-    return database.getEntities().stream()
-        .flatMap((entity) ->
-            Stream.concat(
-                Stream.of(entity.getDdl()),
-                entity.getIndices().stream()
-                    .map(Index::getDdl)
-             )
-              .map((s) -> s.replace(PLACEHOLDER, entity.getName()))
-            );
+    return Stream.concat(
+        database.getEntities().stream()
+            .flatMap((entity) ->
+                Stream.concat(
+                    Stream.of(entity.getDdl()),
+                    entity.getIndices().stream()
+                        .map(Index::getDdl)
+                )
+                    .map((s) -> s.replace(ENTITY_PLACEHOLDER, entity.getName()))
+            ),
+        database.getViews().stream()
+            .map((view) -> view.getDdl().replace(VIEW_PLACEHOLDER, view.getName()))
+    );
   }
+
 }
